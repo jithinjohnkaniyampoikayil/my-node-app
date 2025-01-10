f1b7f2829b67e6f7a8041b83565b9a807b708628
jithinjohnkaniyampoikayil <jithin.john537@gmail.com>
2025-01-10 11:33:41 +0000

    Change server port from 3000 to 8080 and remove port argument from listen method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 
-const port = 3000;
+const port = 8080;
 
 const server = http.createServer((req, res) => {
   if (req.url === "/" && req.method === "GET") {
@@ -14,6 +14,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(port, () => {
+server.listen(() => {
   console.log(`Server running at ${port}/`);
 });
